ecac446e0bb40868bc820017c28c332a58a3e613
fadlynj <fadlynugrahaj@gmail.com>
2024-11-16 19:20:56 +0700

    Modify assets configs

--- lib/core/configs/assets/app_icons.dart
+++ lib/core/configs/assets/app_icons.dart
@@ -1,13 +1,41 @@
 class AppIcons {
-  // Nase path for app icons
+  // Base path for app icons
   static const String _basePath = 'assets/icons/';
   static const String _extension = '.svg';
 
   static const String arrowLeft = '${_basePath}arrow_left$_extension';
-  static const String user = '${_basePath}user$_extension';
-  static const String password = '${_basePath}password$_extension';
+  static const String caretDown = '${_basePath}caret_down$_extension';
+  static const String chat = '${_basePath}chat$_extension';
+  static const String chatFilled = '${_basePath}chat_filled$_extension';
+  static const String envelopeOpen = '${_basePath}envelope_open$_extension';
   static const String eye = '${_basePath}eye$_extension';
   static const String eyeSlash = '${_basePath}eye_slash$_extension';
-  static const String envelopeOpen = '${_basePath}envelope_open$_extension';
+  static const String gear = '${_basePath}gear$_extension';
+  static const String history = '${_basePath}history$_extension';
+  static const String heart = '${_basePath}heart$_extension';
+  static const String heartFilled = '${_basePath}heart_filled$_extension';
+  static const String house = '${_basePath}house$_extension';
+  static const String houseFilled = '${_basePath}house_filled$_extension';
+  static const String list = '${_basePath}list$_extension';
+  static const String magnifyingGlass =
+      '${_basePath}magnifying_glass$_extension';
+  static const String password = '${_basePath}password$_extension';
+  static const String pencil = '${_basePath}pencil$_extension';
+  static const String receipt = '${_basePath}receipt$_extension';
+  static const String receiptFilled = '${_basePath}receipt_filled$_extension';
+  static const String shoppingCart = '${_basePath}shopping_cart$_extension';
+  static const String signOut = '${_basePath}sign_out$_extension';
+  static const String star = '${_basePath}star$_extension';
+  static const String starFilled = '${_basePath}star_filled$_extension';
+  static const String user = '${_basePath}user$_extension';
+  static const String userFocus = '${_basePath}user_focus$_extension';
+  static const String userFocusBold = '${_basePath}user_focus_bold$_extension';
   static const String warningCircle = '${_basePath}warning_circle$_extension';
+  static const String x = '${_basePath}x$_extension';
+  static const String aperture = '${_basePath}aperture$_extension';
+  static const String camera = '${_basePath}camera$_extension';
+  static const String cameraRotate = '${_basePath}camera_rotate$_extension';
+  static const String fileImage = '${_basePath}file_image$_extension';
+  static const String lightning = '${_basePath}lightning$_extension';
+  static const String lightningSlash = '${_basePath}lightning_slash$_extension';
 }
--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -2,6 +2,9 @@ class AppImages {
   // Base path for app images
   static const String _basePath = 'assets/images/';
 
+  static const String imagePlaceholder = '${_basePath}image_placeholder.svg';
   static const String register = '${_basePath}register.svg';
   static const String login = '${_basePath}login.svg';
+  static const String emptyCart = '${_basePath}empty_cart.svg';
+  static const String selfie = '${_basePath}selfie.svg';
 }
